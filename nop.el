b7aece7d740ce0d887088f39179214e88888b094
James S <i@nixeagle.org>
2010-08-05 00:02:40 +0000

    Some helper elisp to test the kernel

---
+++ nop.el
@@ -0,0 +1,58 @@
+;;; nop.el --- Emacs helpers for working on nop.
+;;
+;; Filename: nop.el
+;; Description:
+;; Author: James
+;; Maintainer:
+;; Created: Wed Aug  4 23:46:41 2010 (+0000)
+;; Version:
+;; Last-Updated:
+;;           By:
+;;     Update #: 3
+;; URL:
+;; Keywords:
+;; Compatibility:
+;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;
+;;; Commentary:
+;;
+;;
+;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;
+;;; Change log:
+;;
+;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;
+;; This program is free software; you can redistribute it and/or
+;; modify it under the terms of the GNU General Public License as
+;; published by the Free Software Foundation; either version 3, or
+;; (at your option) any later version.
+;;
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+;; General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with this program; see the file COPYING.  If not, write to
+;; the Free Software Foundation, Inc., 51 Franklin Street, Fifth
+;; Floor, Boston, MA 02110-1301, USA.
+;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;
+;;; Code:
+
+;;; Note that this stuff really does not bother to segregate itself from
+;;; cppmode.
+
+(define-key c++-mode-map (kbd "C-C t")
+  (lambda () (interactive)
+    "Compile and test nos in qemu."
+    (start-process "make-and-test-nos" nil "make"
+                   "-C" "~/cpp/os" "all" "doxygen" "qemu")))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; nop.el ends here
