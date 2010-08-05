7e894b53f6522835cb92534ea539d16562bf04b7
James S <i@nixeagle.org>
2010-08-05 05:55:03 +0000

    More documentation! This time for kmalloc

--- src/kernel/memory/memory.h
+++ src/kernel/memory/memory.h
@@ -7,7 +7,19 @@ extern "C" int kernel_end;
 
 namespace kernel {
   namespace memory {
-    void* __attribute__((alloc_size(1))) kmalloc(size_t size);
+    /** Allocate memory off the end of the kernel.
+
+        \warning This kmalloc has no equivalent free function. All memory
+        allocated cannot be reclaimed by the kernel!.
+
+        \param[in] size of requested memory in bytes.
+
+        \pre Enough memory to satisfy request for \a size bytes.
+
+        \post \li \ref allocated_byte_count is incremented by \a size.
+        \post \li System has allocated dynamic memory memory.
+     */
+    void* __attribute__((alloc_size(1),fastcall,malloc)) kmalloc(size_t size);
     size_t getAllocatedByteCount(void);
   }
 }
