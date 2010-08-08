7a3a12e687a2d0d0ef1c502918e26983aebf7ca6
James S <i@nixeagle.org>
2010-08-08 22:15:09 +0000

    lgdt inline asm corrected and now uses BaseDescriptor

--- src/kernel/asm/lgdt.h
+++ src/kernel/asm/lgdt.h
@@ -1,12 +1,13 @@
 #pragma once
 #include "kernel/gdt/gdt.h"
+#include "kernel/gdt/descriptor.h"
 
 namespace kernel {
   /// Inline assembler
   namespace inlasm {
     __attribute__ ((gnu_inline)) inline void
-    lgdt(kernel::gdt::GdtDescriptor* descriptor) {
-      asm ("lgdtl %0;" : : "m" (descriptor));
+    lgdt(kernel::gdt::BaseDescriptor<kernel::gdt::GdtEntry>* (descriptor)) {
+      asm ("lgdt %0;" : : "m" (descriptor));
     }
   }
 }
