dc590c2dbdc12db86d7e181e841377889af29bc7
James S <i@nixeagle.org>
2010-08-09 19:50:25 +0000

    New isr_common_stub in asm and export isrHandler

--- src/kernel/isr/isr-assembly.asm
+++ src/kernel/isr/isr-assembly.asm
@@ -1,5 +1,5 @@
-[EXTERN isr_common_stub]
-
+extern isrHandler
+bits 32
 %macro ISR_COMMON 2
         [GLOBAL isr%1]
 isr%1:
@@ -24,3 +24,27 @@ isr%1:
         ISRS 9, 1, 0            ; 9 no error
         ISRS 10, 5, 1           ; 10..14 error
         ISRS 15, 16, 0          ; 15..31 no error
+
+isr_common_stub:
+        pusha                   ; Pushes edi,esi,ebp,esp,ebx,edx,ecx,eax
+        push ds
+        push es
+        push fs
+        push gs
+        mov ax, 0x10            ; Load the Kernel Data Segment descriptor!
+        mov ds, ax
+        mov es, ax
+        mov fs, ax
+        mov gs, ax
+        mov eax, esp            ; Push us the stack
+        push eax
+        mov eax, isrHandler
+        call eax            ; A special call, preserves the 'eip' register
+        pop eax
+        pop gs
+        pop fs
+        pop es
+        pop ds
+        popa
+        add esp, 8 ; Cleans up the pushed error code and pushed ISR number
+        iret       ; pops 5 things at once: CS, EIP, EFLAGS, SS, and ESP!
