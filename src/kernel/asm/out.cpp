3a8f3a84db076fe4b3debf93ba750f5186e96483
James S <i@nixeagle.org>
2010-08-10 22:06:24 +0000

    move implementation to header for asm/out as these are showing up as undefined.
    I really don't know why gcc can't find these when declared in their own
    source file, but for the time being putting them in the header file
    accomplishes the same thing.

--- src/kernel/asm/out.cpp
+++ src/kernel/asm/out.cpp
@@ -2,16 +2,16 @@
 
 namespace kernel {
   namespace inlasm {
-    void outb (uint8_t port, uint8_t value) {
-      asm volatile("outb %0,%1"::"a"(value), "Nd" (port));
-    }
+    // static inline void outb (uint16_t port, uint8_t value) {
+    //   asm volatile("outb %0,%1"::"a"(value), "Nd" (port));
+    // }
 
-    void outw (uint8_t port, uint16_t value) {
-      asm volatile("outw %0,%1"::"a"(value), "Nd" (port));
-    }
+    // void outw (uint16_t port, uint16_t value) {
+    //   asm volatile("outw %0,%1"::"a"(value), "Nd" (port));
+    // }
 
-    void outl (uint8_t port, uint32_t value) {
-      asm volatile("outl %0,%1"::"a"(value), "Nd" (port));
-    }
+    // void outl (uint16_t port, uint32_t value) {
+    //   asm volatile("outl %0,%1"::"a"(value), "Nd" (port));
+    // }
   }
 }
--- src/kernel/asm/out.h
+++ src/kernel/asm/out.h
@@ -1,12 +1,20 @@
+#pragma once
 #include "types.h"
 namespace kernel {
   namespace inlasm {
     /// Output a \a byte to \a port.
-    static inline void outb (uint8_t port, uint8_t value);
+    static inline void outb (uint16_t port, uint8_t value) {
+      asm volatile("outb %0,%1"::"a"(value), "Nd" (port));
+    }
+
     /// Output a \a word to \a port.
-    static inline void outw (uint8_t port, uint16_t value);
+    static inline void outw (uint16_t port, uint16_t value) {
+      asm volatile("outw %0,%1"::"a"(value), "Nd" (port));
+    }
     /// Output a \a word to \a port.
-    static inline void outl (uint8_t port, uint32_t value);
+    static inline void outl (uint16_t port, uint32_t value) {
+      asm volatile("outl %0,%1"::"a"(value), "Nd" (port));
+    }
 
   }
 }
