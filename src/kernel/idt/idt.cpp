17aea2a18a5efcab6877c6eca98f8ca612aa783a
James S <i@nixeagle.org>
2010-08-10 04:05:40 +0000

    interrupt handler set for int 0x3 only

--- src/kernel/idt/idt.cpp
+++ src/kernel/idt/idt.cpp
@@ -2,6 +2,7 @@
 #include "kernel/gdt/descriptor.h"
 #include "kernel/panic/kpanic.h"
 #include "kernel/asm/lidt.h"
+#include "kernel/isr/isr.h"
 
 namespace kernel {
   namespace idt {
@@ -12,12 +13,20 @@ namespace kernel {
     //    }
 
     using kernel::gdt::BaseDescriptor;
+
     BaseDescriptor<IdtEntry> init(uint16_t entry_count) {
       if (256 < entry_count) {
         KPANIC("entry_count larger then 256", "Positive overflow");
       } else {
-        BaseDescriptor<IdtEntry> idt = BaseDescriptor<IdtEntry>(entry_count);
-        kernel::inlasm::lidt(&idt);
+        BaseDescriptor<IdtEntry> idt = BaseDescriptor<IdtEntry>(256);
+
+        //        kernel::text_mode::put_hex((size_t)idt, 2, 40);
+        idt.getBase()[3].setBase(reinterpret_cast<uint32_t>(&isr3));
+        idt.getBase()[3].setSelector(0x08);
+        idt.getBase()[3].setAccessByte(0x8E);
+
+        asm("lidt %0" : : "m" (idt));
+        //        kernel::inlasm::lidt(&idt);
         return idt;
       }
     }
