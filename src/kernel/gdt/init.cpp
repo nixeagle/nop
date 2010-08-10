7d01b7bb11dbb25ba3bf8ca9a1721a4ee98376d9
James S <i@nixeagle.org>
2010-08-10 04:07:07 +0000

    Vastly expand the inline assembly for lgdt to set all the segments to 0x10 other then cs.

--- src/kernel/gdt/init.cpp
+++ src/kernel/gdt/init.cpp
@@ -1,13 +1,15 @@
+
 #include "gdt.h"
 #include "descriptor.h"
-#include "kernel/asm/lgdt.h"
+//#include "kernel/asm/lgdt.h"
+
 namespace kernel {
   namespace gdt {
-
+    BaseDescriptor<GdtEntry> descs = BaseDescriptor<GdtEntry>(3);
     BaseDescriptor<GdtEntry> init(void) {
       using kernel::gdt::AccessOptions;
       using kernel::gdt::FlagOptions;
-      BaseDescriptor<GdtEntry> descs = BaseDescriptor<GdtEntry>(3);
+
 
       descs.getBase()[1].setLimit(0xfffff);
       descs.getBase()[1].setAccessByte(AccessOptions::Executable
@@ -25,8 +27,22 @@ namespace kernel {
       descs.getBase()[2].setFlags(FlagOptions::Granularity
                                   | FlagOptions::Size);
 
-      kernel::inlasm::lgdt(&descs);
-
+      ///@todo get kernel::inlasm inline assembly namespace up and running
+      ///again
+      //kernel::inlasm::lgdt(&descs);
+      asm volatile ("lgdt %0;"
+                    ".intel_syntax noprefix ;"
+                    "jmp 0x8:.flush ;"
+                    ".flush: ;"
+                    "mov ax, 0x10 ;"
+                    "mov ds, ax ;"
+                    "mov es, ax ;"
+                    "mov fs, ax ;"
+                    "mov gs, ax ;"
+                    "mov ss, ax ;"
+                    ".att_syntax prefix ;"
+                    : : "m" (descs)
+                    : "ax");
       /** \retval GdtDescriptor is a fully loaded and initialized
           descriptor. The return result can be used for printing
           information about the gdt out for debugging.
