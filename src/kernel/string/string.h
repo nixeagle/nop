bedd5ae69beaf0de7e4b00a58fc1134e29eedbd9
James S <i@nixeagle.org>
2010-08-14 01:33:31 +0000

    comment out the non class member functions** and fix errors to new class funcs, thanks zc00gii.
    **these look good, but I want nop to compile.

--- src/kernel/string/string.h
+++ src/kernel/string/string.h
@@ -28,9 +28,10 @@ namespace kernel {
       }
       int toInt() {
         int number = 0;
-        for(int i = this->length(), int power=1; i>=0; i++, power*=10) {
+        for(int i = this->length(),  power=1; i>=0; i++, power*=10) {
           number+=(this->string[i] - 0x30) * power;
         }
+        return number;
       }
     private:
       /// @todo Make accessor function for the length.
@@ -38,27 +39,27 @@ namespace kernel {
     public:
       char* string;
     };
-    String strcat(String *string1, String *string2) {
-      char temp_c_string[string1->length() + string2->length() + 1];
-      int indice1=0;
-      int indice2=0;
-      while(string1->string[indice1]!="\0") {
-        temp_c_string[indice2] = string1->string[indice1];
-        indice1++;
-        indice2++;
-      }
-      indice1=0;
-      while(string2->string[indice1]!="\0") {
-        temp_c_string[indice2] = string1->string[indice1];
-        indice1++;
-        indice2++;
-      }
-      String *tempString = new String(temp_c_string);
-      return *tempString;
-    }
-    int str2int(String *string) {
-      return string->toInt();
-    }
+    // String strcat(String *string1, String *string2) {
+    //   char temp_c_string[string1->length() + string2->length() + 1];
+    //   int indice1=0;
+    //   int indice2=0;
+    //   while(string1->string[indice1]!="\0") {
+    //     temp_c_string[indice2] = string1->string[indice1];
+    //     indice1++;
+    //     indice2++;
+    //   }
+    //   indice1=0;
+    //   while(string2->string[indice1]!="\0") {
+    //     temp_c_string[indice2] = string1->string[indice1];
+    //     indice1++;
+    //     indice2++;
+    //   }
+    //   String *tempString = new String(temp_c_string);
+    //   return *tempString;
+    // }
+    // int str2int(String *string) {
+    //   return string->toInt();
+    // }
     // string make_string(const char* string);
     //      Length of a string, C and pascal styles respectively.
   }
