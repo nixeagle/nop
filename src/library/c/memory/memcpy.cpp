d01cc071499d6c3066dd19e8890ad17eddddaa3f
James S <i@nixeagle.org>
2010-08-15 05:11:05 +0000

    C++ memcpy looks very ugly due to "new" style casting :<

--- src/library/c/memory/memcpy.cpp
+++ src/library/c/memory/memcpy.cpp
@@ -2,9 +2,12 @@
 
 namespace c {
   void *memcpy(void *destination, const void *source, size_t n) {
-    for(int i = 0; i < n; i++) {
-      destination[i] = source[i];
+    char* temp_dest = reinterpret_cast<char*>(destination);
+    char* temp_source = reinterpret_cast<char*>(const_cast<void*>(source));
+    for(size_t i = 0; i < n; i++) {
+      *temp_dest++ = *temp_source++;
     }
+    /// \retval pointer to destination
     return destination;
   }
 }
