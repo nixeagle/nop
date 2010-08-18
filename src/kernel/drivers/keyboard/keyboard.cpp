15796cefa731e53f538180ff4f267cca90769076
James S <i@nixeagle.org>
2010-08-18 23:08:58 +0000

    switch to using KeyEvent objects for numeric output (for now)

--- src/kernel/drivers/keyboard/keyboard.cpp
+++ src/kernel/drivers/keyboard/keyboard.cpp
@@ -16,36 +16,41 @@ namespace kernel {
       using kernel::text_mode::puts;
       using kernel::text_mode::put_hex;
       using kernel::text_mode::put_char;
+      using kernel::VirtualConsole;
       static uint32_t keypress_number = 0;
       void handleEvent() {
         uint8_t scan_code = inb(0x60);
 
-        static bool shift = false; // state of the shift key.
-        static bool control = false; // state of the control key.
+        // static bool shift = false; // state of the shift key.
+        // static bool control = false; // state of the control key.
 
-        switch(scan_code) {
-        case 0x36: // right shift
-        case 0x2a: // left shift
-          shift = true;
-          break;
-        case 0xb6: // Right shift break
-        case 0xaa: // left shift break
-          shift = false;
-          break;
-        default:
-          puts("Last 8 scancodes", 11, 0);
-          put_hex(scan_code, 12, (keypress_number++ % 8) * 10);
-          if(shift) {
-            put_char(keysym_us_shift[scan_code], 10, 30);
-          } else {
-            put_char(keysym_us[scan_code], 10, 30);
-          }
-          KeyEvent* a = new KeyEvent();
-          (kernel::global::key_event)(kernel::VirtualConsole::currentConsole(),
-                                      a);
+        // switch(scan_code) {
+        // case 0x36: // right shift
+        // case 0x2a: // left shift
+        //   shift = true;
+        //   break;
+        // case 0xb6: // Right shift break
+        // case 0xaa: // left shift break
+        //   shift = false;
+        //   break;
+        // default:
+        //   //          puts("Last 8 scancodes", 11, 0);
+        //   //          put_hex(scan_code, 12, (keypress_number++ % 8) * 10);
+        //   //          KeyEvent* event;
+        //   //          if(shift) {
+        //     //event = new KeyEvent(scan_code, shift, control);
+        //     //            put_char(keysym_us_shift[scan_code], 10, 30);
+        //   //          } else {
+        //     //event = new KeyEvent(scan_code, shift, control);
+        //     // put_char(keysym_us[scan_code], 10, 30);
+        //   //          }
+        //   // KeyEvent* a = new KeyEvent(scan_code, shift, control);
 
-          break;
-        }
+        //   break;
+        // }
+        KeyEvent* event = new KeyEvent(scan_code);
+        (kernel::global::key_event)(VirtualConsole::currentConsole(), event);
+        delete event;
       }
     }
   }
