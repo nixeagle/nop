8c4298fae1fe215d3b6b3180aaed4751ce86e724
James S <i@nixeagle.org>
2010-08-18 23:09:23 +0000

    Bit field containing modifier keys.

--- src/kernel/drivers/keyboard/key-event.h
+++ src/kernel/drivers/keyboard/key-event.h
@@ -7,14 +7,28 @@ namespace kernel {
 
       /** Keyboard event */
       class KeyEvent {
-        /// Was shift pressed?
-        bool shift;
-        /// Was control pressed?
-        bool control;
+        static struct {
+          /// Was shift pressed?
+          /// \pre Assumes only one keyboard is attached.
+          bool shift : 1;
+          /// \pre Assumes only one keyboard is attached.
+          /// Was control pressed?
+          bool control : 1;
+          bool caps_lock : 1;
+          bool num_lock : 1;
+          bool meta : 1;
+          bool super : 1;
+          bool menu : 1;
+          bool scroll_lock : 1;
+        } modifier;
         /// keysym pressed.
         uint32_t code;
       public:
-        KeyEvent() : shift(false), control(false), code(0) {}
+        KeyEvent(uint32_t code)
+          : code(code) {}
+        char getAsciiKey(void) const {
+          return static_cast<char>(code);
+        }
       };
     }
   }
