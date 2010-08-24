9c019922bdcc6229f497b54142be2f4b734b5a85
James S <i@nixeagle.org>
2010-08-24 00:33:12 +0000

    Use case ranges to reduce 24 lines of code to just 4 \o/

--- src/kernel/virtual-console/handle-key.cpp
+++ src/kernel/virtual-console/handle-key.cpp
@@ -16,28 +16,8 @@ namespace kernel {
       switch (event->getCode()) {
       case 0xFFFFFFFF: // Key event was a modifier key.
         break;
-      case 0x3b:
-        kernel::global::virtual_consoles[0].setCurrent();
-        out = *VirtualConsole::currentConsole();
-        break;
-      case 0x3c:
-        kernel::global::virtual_consoles[1].setCurrent();
-        out = *VirtualConsole::currentConsole();
-        break;
-      case 0x3d:
-        kernel::global::virtual_consoles[2].setCurrent();
-        out = *VirtualConsole::currentConsole();
-        break;
-      case 0x3e:
-        kernel::global::virtual_consoles[3].setCurrent();
-        out = *VirtualConsole::currentConsole();
-        break;
-      case 0x3f:
-        kernel::global::virtual_consoles[4].setCurrent();
-        out = *VirtualConsole::currentConsole();
-        break;
-      case 0x40:
-        kernel::global::virtual_consoles[5].setCurrent();
+      case 0x3b ... 0x40:
+        kernel::global::virtual_consoles[event->getCode() - 0x3b].setCurrent();
         out = *VirtualConsole::currentConsole();
         break;
       case 0x41: // F7
