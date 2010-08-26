3e19525d431895c018f77be303fddc792b38f442
James S <i@nixeagle.org>
2010-08-26 19:39:59 +0000

    kpanic never returns, tell the compiler this to shut it up ;).

--- src/kernel/gdt/panic.cpp
+++ src/kernel/gdt/panic.cpp
@@ -2,12 +2,16 @@
 #include "kernel/panic/kpanic.h"
 namespace kernel {
   namespace panic {
+    __attribute__((noreturn)) inline static void halt () {
+      asm volatile ("hlt");
+      __builtin_unreachable();
+    }
     void kpanic (kernel::gdt::GdtDescriptor* gdt_descriptor,
                  const char* message, const char* function,
                  int line, const char* file, const char* timestamp) {
       header(message, function, line, file, timestamp);
       gdt_descriptor->inspect(6);
-      asm("hlt");
+      halt();
     }
 
     void kpanic (kernel::gdt::GdtEntry* gdt_entry,
@@ -15,7 +19,7 @@ namespace kernel {
                  int line, const char* file, const char* timestamp) {
       header(message, function, line, file, timestamp);
       gdt_entry->inspect(6);
-      asm("hlt");
+      halt();
     }
   }
 }
