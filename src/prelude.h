a8fa952278558b49f35d51b6a091084f467f2bb4
James S <i@nixeagle.org>
2011-12-16 01:38:22 -0500

    New prelude header which will be auto included
    Thanks to scott for this idea.

---
+++ src/prelude.h
@@ -0,0 +1,9 @@
+#pragma once
+
+// WARNING! This is automatically included in all other files.
+// We do this for simplicity and to avoid repeating some common includes
+// over and over.
+
+// Up first is our type definitions. Everything in the project is based on
+// having access to these 'simple' types.
+#include "types.h"
