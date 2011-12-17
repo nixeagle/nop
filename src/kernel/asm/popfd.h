f2b222d2e1a6501f6c72d4fcda210ba4a175fddb
James S <i@nixeagle.org>
2011-12-17 14:45:26 -0500

    New inline assembly instruction for setting the flags register.

---
+++ src/kernel/asm/popfd.h
@@ -0,0 +1,15 @@
+#pragma once
+#include "types.h"
+
+namespace kernel {
+  namespace as {
+    /// Set the flags register from the stack, popping the value.
+    /// For C++ compatability we also push the value onto the stack before
+    /// executing this assembly instruction. The resulting machine state of
+    /// the stack is unmodified after this instruction.
+    inline void popfd(u32 flags) {
+      asm volatile("push %0;popfd": "=r"(flags));
+      return;
+    }
+  }
+}
