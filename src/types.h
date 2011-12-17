65e700f127cc579c724e80399c5a2164a81e84c5
James S <i@nixeagle.org>
2011-12-16 22:59:24 -0500

    New C++11 integer types added plus useful shorthands.

--- src/types.h
+++ src/types.h
@@ -12,12 +12,31 @@
 // I think these are the same on 32bit and 64 bit x86 machines...
 typedef unsigned char uint8_t;
 typedef unsigned short int uint16_t;
-typedef unsigned int uint32_t;
+typedef unsigned int uint32_t; // TODO: Are we sure?
+typedef signed char int8_t;
+typedef signed short int int16_t;
+typedef signed int int32_t;
 
 #ifdef __x86_64
 typedef unsigned long int uint64_t;
+typedef signed long long int int64_t;
+typedef unsigned uint64_t u64;
+typedef signed int64_t s64;
 #endif
 
 #ifdef __x86_64
 #elif __i386
 #endif
+
+// Define some nice shorthand typenames in terms of the C++11 types.
+typedef uint8_t u8;
+typedef uint16_t u16;
+typedef uint32_t u32;
+typedef int8_t s8;
+typedef int16_t s16;
+typedef int32_t s32;
+
+/// Short for an unsigned machine word.
+typedef unsigned long int uint;
+/// Short for a signed machine word.
+typedef unsigned long int sint;
